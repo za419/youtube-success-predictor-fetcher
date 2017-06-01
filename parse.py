854f53a77528493d5bb14722090d44d8e5da3738
Alan <alanwoj16@gmail.com>
2017-06-01 18:20:45 -0500

    channel info in csv

--- parse.py
+++ parse.py
@@ -1,6 +1,25 @@
-import json
+import json, csv
 
 with open('output.json') as json_data:
     d = json.load(json_data, strict=False)
-    for e in d:
-        print e
+    with open('data.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        header = []
+        header.append("topicIds")
+        header.append("topicCategories")
+        header.append("commentCount")
+        header.append("viewCount")
+        header.append("videoCount")
+        header.append("subscriberCount")
+        header.append("hiddenSubscriberCount")
+        writer.writerow(header)
+        for sub in d:
+            header = []
+            header.append(sub['topicDetails']['topicIds'])
+            header.append(sub['topicDetails']['topicCategories'])
+            header.append(sub['statistics']['commentCount'])
+            header.append(sub['statistics']['viewCount'])
+            header.append(sub['statistics']['videoCount'])
+            header.append(sub['statistics']['subscriberCount'])
+            header.append(sub['statistics']['hiddenSubscriberCount'])
+            writer.writerow(header)
